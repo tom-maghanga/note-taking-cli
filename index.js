22b7e5f49cb143096b3052c56ee6b2a808e9cd89
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-01-19 06:13:26 +0300

    Proccessing CLI Arguments

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const note = process.argv[2];
+#!/usr/bin/env node
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
 
-const newNote = {
-    "content" : note,
-    "Date" : Date.now()
-};
 
-console.log(newNote);
+yargs(hideBin(process.argv))
+  .command('new <note>', 'Create a new Note', (yargs) => {
+    // console.log(process.argv);
+    return yargs
+      .positional('note', {
+        type : 'string',
+        description: 'The content of the note to create',
+        
+      })
+  }, (argv) => {
+  
+    console.log(argv.note)
 
+  })
+  .demandCommand(1)
+  .parse()
